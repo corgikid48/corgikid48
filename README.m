4c6bd19e67e12e1c7f0ea75d1d897ce7456c5254
CORGIBOI <144067726+corgikid48@users.noreply.github.com>
2023-09-19 18:02:20 -0700

    Create README.m

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m ZaneC/Gabriel (account is corgikid48)
+- 👀 I’m interested in art
+- 🌱 I’m currently learning about history in middle school
+- 💞️ I’m looking to collaborate on how to create a game/project on this platform
+- 📫 How to reach me by E-mail at gabriel53854@wintersjusd.org
+
+<!---
+corgikid48/corgikid48 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
